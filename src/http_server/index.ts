a303295178a58b39675ca43315b037e53d494e32
amberlynn <amberlynn364@gmail.com>
2024-02-19 12:20:30 +0300

    feat: add project dependecies

---
+++ src/http_server/index.ts
@@ -0,0 +1,20 @@
+/* eslint-disable import/prefer-default-export */
+import * as fs from 'fs';
+import * as path from 'path';
+import * as http from 'http';
+
+// eslint-disable-next-line no-underscore-dangle, @typescript-eslint/naming-convention
+const __dirname = path.resolve(path.dirname(''));
+
+export const httpServer = http.createServer((req, res) => {
+  const filePath = path.join(__dirname, req.url === '/' ? '/front/index.html' : `/front${req.url}`);
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.writeHead(404);
+      res.end(JSON.stringify(err));
+      return;
+    }
+    res.writeHead(200);
+    res.end(data);
+  });
+});
