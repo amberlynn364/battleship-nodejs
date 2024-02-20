ab4e558667a5b8c21af662d4effbe2fa59d4b0de
amberlynn <amberlynn364@gmail.com>
2024-02-20 19:02:25 +0300

    feat: implement web socker server

--- src/servers/interfaces.ts
+++ src/servers/interfaces.ts
@@ -1,5 +1,23 @@
 import { type Server } from 'http';
 
+export interface WsMessage<T = unknown> {
+  type: string;
+  data: T;
+  id: 0;
+}
+
+export interface WsContext {
+  id: number;
+  send: (message: WsMessage | WsMessage[]) => void;
+  broadcast: (message: WsMessage | WsMessage[], conn?: number | number[]) => void;
+}
+
+export interface WsController {
+  handleMessage: (message: WsMessage, context: WsContext) => void;
+  handleClose: (context: WsContext) => void;
+}
+
 export interface WsServerDeps {
   server: Server;
+  controller: WsController;
 }
--- src/servers/wsServer.ts
+++ src/servers/wsServer.ts
@@ -1,33 +1,97 @@
-import { WebSocket, WebSocketServer } from 'ws';
-import { type WsServerDeps } from './interfaces.js';
+import { type RawData, type WebSocket, WebSocketServer } from 'ws';
+import { type WsContext, type WsMessage, type WsController, type WsServerDeps } from './interfaces.js';
+import { randomID } from '../utils/randomID.js';
+import { parseMessage, stringifyMessage } from '../utils/messageUtils.js';
 
 export class WsServer extends WebSocketServer {
-  constructor({ server }: WsServerDeps) {
+  private readonly controller: WsController;
+
+  private readonly clientsMap = new Map<number, WebSocket>();
+
+  constructor({ server, controller }: WsServerDeps) {
     super({ server });
-
-    this.setupWebSocket();
+    this.controller = controller;
+    this.on('connection', this.onConnection);
+    this.on('close', this.onClose);
   }
 
-  private setupWebSocket(): void {
-    this.on('connection', (socket: WebSocket) => {
-      console.log('Client connected to WebSocket');
+  public broadcast = (message: WsMessage | WsMessage[], contextID?: WsContext['id'] | Array<WsContext['id']>): void => {
+    try {
+      const messages = Array.isArray(message) ? message : [message];
+      messages.forEach((msg) => {
+        const rawMessage = stringifyMessage(msg);
+        console.log(rawMessage);
 
-      socket.on('message', (message: string) => {
-        console.log(`Received message: ${message}`);
-        this.broadcastMessage(message, socket);
-      });
+        let clients;
+        if (contextID === undefined) {
+          clients = this.clientsMap;
+        } else if (Array.isArray(contextID)) {
+          clients = this.getConnectionsByID(contextID);
+        } else {
+          clients = this.getConnectionsByID([contextID]);
+        }
 
-      socket.on('close', (message: string) => {
-        console.log('Client disconnected from WebSocket');
+        clients.forEach((client) => {
+          client.send(rawMessage);
+        });
       });
+    } catch (error) {
+      const errorMessage = error instanceof Error ? error.message : 'Unknown error';
+      console.error(errorMessage);
+    }
+  };
+
+  private readonly onConnection = (ws: WebSocket): void => {
+    const context = this.createWebSocketContext(ws);
+    console.log(`client connected ${context.id}`);
+    ws.on('message', this.onConnectionMessage(context));
+    ws.on('close', () => {
+      this.onConnectionClose(context);
     });
+  };
+
+  private readonly onClose = (): void => {
+    console.log('Server closed');
+    this.clientsMap.forEach((ws) => {
+      ws.close();
+    });
+  };
+
+  private readonly onConnectionMessage = (context: WsContext) => (rawMessage: RawData) => {
+    try {
+      console.log(String(rawMessage));
+      const message = parseMessage(rawMessage);
+      this.controller.handleMessage(message, context);
+    } catch (error) {
+      const message = error instanceof Error ? error.message : 'Unknown error';
+      console.error(message);
+    }
+  };
+
+  private readonly onConnectionClose = (context: WsContext): void => {
+    const { id } = context;
+    this.controller.handleClose(context);
+    this.clientsMap.delete(id);
+    console.log(`Client disconnected ${id}`);
+  };
+
+  private createWebSocketContext(ws: WebSocket): WsContext {
+    const id = randomID();
+    this.clientsMap.set(id, ws);
+    return {
+      id,
+      send: (message) => {
+        this.broadcast(message, id);
+      },
+      broadcast: this.broadcast,
+    };
   }
 
-  private broadcastMessage(message: string, sender: WebSocket): void {
-    this.clients.forEach((client) => {
-      if (client !== sender && client.readyState === WebSocket.OPEN) {
-        client.send(message);
-      }
-    });
+  private getConnectionsByID(ids: number[] = []): WebSocket[] {
+    return ids.reduce<WebSocket[]>((acc, id) => {
+      const client = this.clientsMap.get(id);
+      if (client) acc.push(client);
+      return acc;
+    }, []);
   }
 }
